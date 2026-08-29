49fb41c9f1b790762f14575f0b0b624786c3c9cf
gyaroron <gyaroron@gmail.com>
2026-08-30 06:21:57 +0900

    solved_20260830

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -33,7 +33,15 @@ def bubble_sort(arr):
         정렬된 배열
     """
     n = len(arr)
-    
+    ##### 최초의 ptr이 도달할 수 있는 범위는 크기가 n인 배열에서 n-2번 까지 
+
+    #### python의 range(n)는 0 부터 n - 1 까지(n 미포함)
+    #### reversed()를 이용하여 n - 1 부터 1 까지(n 미포함)
+    #### nested for loop 구조를 이용하여 ptr이 0 부터 n - 2 까지 이동 
+    for k in reversed(range(1,n)):
+        for ptr in range(k):
+            if arr[ptr] > arr[ptr + 1]:
+                arr[ptr], arr[ptr + 1] = arr[ptr + 1], arr[ptr]    
     # TODO: 외부 반복문 - n-1번 반복
     # 각 패스마다 가장 큰 원소가 끝으로 이동
     ## TODO: 내부 반복문 - 인접한 원소 비교
@@ -41,7 +49,7 @@ def bubble_sort(arr):
     ## TODO: 인접한 두 원소 비교 및 교환
     ## arr[j] > arr[j+1]이면 교환
     ## 외부 반복문: n-1번 실행
-    pass
+
         
     return arr
 
@@ -56,18 +64,20 @@ def bubble_sort_optimized(arr):
         정렬된 배열
     """
     n = len(arr)
-    
-    for i in range(n):
-        swapped = False  # 교환 발생 여부
-        
-        # TODO: 내부 반복문과 교환 로직 구현
-        # 교환이 발생하면 swapped = True 설정        
-        pass
-        
+    swapped = False  # 교환 발생 여부
+
+    # TODO: 내부 반복문과 교환 로직 구현 
+    for pass_num in range(n - 1):
+        for idx in range((n - 1) - pass_num):
+            if arr[idx] > arr[idx + 1]:
+                arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+
+                # 교환이 발생하면 swapped = True 설정
+                swapped = True
 
         # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
-
+        if not swapped:
+            break
     return arr
 
 # 테스트 케이스
